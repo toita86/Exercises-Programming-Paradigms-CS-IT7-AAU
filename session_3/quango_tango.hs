daab7b5154e608e05d155247c8f775487257f7e6
Eduard Brahas <eduardbrhas@outlook.it>
2025-10-07 16:39:01 +0200

    [ADD] third session exs

---
+++ session_3/quango_tango.hs
@@ -0,0 +1,6 @@
+quango :: a -> [a]
+quango a = [a]
+
+tango :: Num p1 => (a, b) -> p2 -> p1
+tango (a,b) p2 = 44
+
---
+++ session_3/second_part.hs
@@ -0,0 +1,7 @@
+twice f x = f(f(x))
+
+twicetwo (f,x) = f(f(x))
+
+dingo (x,y) = [x,y]
+
+dingotwo [x,y] = (x,)
